70782503c9e8fe8468b9503c47a9a8c3b53c0c12
Liz <liz.montesano@gmail.com>
2023-08-26 17:01:26 -0400

    new route

--- app/routes.py
+++ app/routes.py
@@ -12,3 +12,10 @@ def index():
             item = json.loads(line)
             items.append(item)
     return render_template('index.html', items=items)
+@app.route('/data')
+    items = []
+    with open('app/static/data.jsonl') as f:
+        for line in f:
+            item = json.loads(line)
+            items.append(item)
+    return render_template('json.html', items=items)
